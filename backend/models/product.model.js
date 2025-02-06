23d9e130498c6a4714a275c8a2c1891d1c202c50
Sushila Kandel <kandel.sushila242@gmail.com>
2025-02-06 11:47:52 +0200

    productSchema added in the productmodel file

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -4,6 +4,15 @@ const productSchema = new mongoose.Schema({
   name:{
     type: String,
     required: true
-},
-
-})
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true //createdAt, updatedAt
+});
