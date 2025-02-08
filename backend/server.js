97fe7ffb2910b28840bcf0d1ad1a47c3e2930527
Sushila Kandel <kandel.sushila242@gmail.com>
2025-02-08 22:16:58 +0200

    controller file added and other files modified

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js"
 
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // allows to accept JSON data in the req.body
 
-app.use("/api/prodcuts", productRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
 	connectDB();
