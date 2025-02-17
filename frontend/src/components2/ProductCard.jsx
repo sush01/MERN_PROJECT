37bd22631c0c47b4fc19d5c9e4494b2218d91c4d
Sushila Kandel <kandel.sushila242@gmail.com>
2025-02-18 00:30:11 +0200

    add handleDeleteProduct and updateProduct and update and cancel button in ProductCard.jsx file

--- frontend/src/components2/ProductCard.jsx
+++ frontend/src/components2/ProductCard.jsx
@@ -1,13 +1,65 @@
 
-import {Box, HStack, Image, IconButton, Text, Heading, useColorModeValue, } from '@chakra-ui/react';
+import {Box, HStack, Image, IconButton, Text, Heading, useColorModeValue, useToast, useDisclosure, Modal, ModalOverlay, ModalContent, ModalCloseButton, VStack, Button} from '@chakra-ui/react';
 import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
+import { useProductStore } from '@/store/product';
+import { useState} from "react";
 
 
 
 const ProductCard = ({product}) => {
 
+  const [updatedProduct, setUpdatedProduct] = useState(product);
+
   const textColor= useColorModeValue("gray.600", "gray.200");
   const bg= useColorModeValue("white", "gray.800");
+
+  const {deleteProduct, updateProduct} = useProductStore();
+  const toast = useToast();
+  const {isOpen, onOpen, onClose }= useDisclosure();
+
+  const handleDeleteProduct = async (pid) => {
+    const {success, message} = await deleteProduct(pid);
+    if(!success){
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable:true,
+
+      });
+    }
+  };
+
+  const handleUpdateProduct = async (pid, updatedProduct) => {
+		const { success, message } = await updateProduct(pid, updatedProduct);
+		onClose();
+		if (!success) {
+			toast({
+				title: "Error",
+				description: message,
+				status: "error",
+				duration: 3000,
+				isClosable: true,
+			});
+		} else {
+			toast({
+				title: "Success",
+				description: "Product updated successfully",
+				status: "success",
+				duration: 3000,
+				isClosable: true,
+			});
+		}
+	};
 
   return (
     <Box
@@ -30,12 +82,55 @@ const ProductCard = ({product}) => {
         </Text>
 
         <HStack spacing= {2}>
-          <IconButton icon={<EditIcon/>} colorScheme='blue'/>
-          <IconButton icon={<DeleteIcon/>} colorScheme='red'/>
+          <IconButton icon={<EditIcon/>}  onClick= {onOpen} colorScheme='blue'/>
+          <IconButton icon={<DeleteIcon/>} onClick= {handleDeleteProduct(product._id)} colorScheme='red'/>
         </HStack>
       </Box>
-    </Box>  
 
-  )
+      <Modal isOpen = {isOpen} onClose={onClose}>
+        <ModalOverlay/>
+
+        <ModalContent>
+          <ModalHeader> Update Product</ModalHeader>
+          <ModalCloseButton/>
+          <ModalBody>
+            <VStack spacing= {4}>
+              <Input 
+              placeholder= "Product Name"
+              name= "name"
+              value= {updatedProduct.name}
+              onChange={(e) => setUpdatedProduct({...updatedProduct, name: e.target.value})}
+              />
+
+              <Input 
+              placeholder= "Price"
+              name= "price"
+              type= "number"
+              value= {updatedProduct.price}
+              onChange={(e) => setUpdatedProduct({...updatedProduct, price: e.target.value})}
+              />
+
+              <Input 
+              placeholder= "Image URL"
+              name= "image"
+              value= {updatedProduct.image}
+              onChange={(e) => setUpdatedProduct({...updatedProduct, image: e.target.value})}
+              />
+
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr= {3} onClick={() => handleUpdateProduct(product._id, updateProduct)}>
+              Update
+            </Button>
+            <Button variant= "ghost" onclick= {onClose}> 
+              Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+          </Modal>
+          </Box>
+          );
 };
 export default ProductCard;
